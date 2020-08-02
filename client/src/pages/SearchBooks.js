3cbe5cd4a8bfbf5265271135888a37765fe3652c
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-08-02 13:27:56 -0700

    fixin save book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,51 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK } from '../utils/mutations';
-import {useMutation} from '@apollo/react-hooks';
-import { QUERY_BOOKS, GET_ME} from '../utils/queries';
+
+
+// Use the Apollo useMutation() Hook to execute the SAVE_BOOK mutation in the handleSaveBook() function instead of the saveBook() function imported from the API file.
+
+// Make sure you keep the logic for saving the book's ID to state in the try...catch block!
 
 const SearchBooks = () => {
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  const [saveBook, {error}] = useMutation(SAVE_BOOK );
-  console.log(error);
-
-//   const [saveBook, {error}] = useMutation(SAVE_BOOK, {
-//     update(cache, {data: {saveBook}}) {
-
-//         try {
-//         //read what's currently in cache 
-//         const { books } = cache.readQuery({ query: QUERY_BOOKS});
-
-//         //prepend the newest thought to the front of the array
-//         cache.writeQuery({
-//             query: QUERY_BOOKS,
-//             data: { books: [saveBook] }, ...books]}
-//         })
-
-//         } catch (e) {
-//             console.error(e)
-//         }
-
-//         //update the object's cache, appending new thought to the end of the array
-//         const { me } = cache.readQuery({query: QUERY_ME});
-//         cache.writeQuery({
-//             query: GET_ME,
-//             data: {me: {...me, books: [...me.nooks, saveBook]}}
-//         })
-//     }
-// })
-
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -89,9 +63,7 @@ const SearchBooks = () => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
- 
+    const bookInput = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -101,19 +73,18 @@ const SearchBooks = () => {
     }
 
     try {
-      // const response = await saveBook(bookToSave, token);
-      const {data} = await saveBook({
-        variables: { input: bookToSave }
+      const { data } = await saveBook({
+        variables: { input: bookInput }
       });
+      console.log(bookInput);
       console.log(data);
 
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
+      if (!data) {
+        throw new Error('something went wrong!');
+      }
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      setSavedBookIds([...savedBookIds, bookInput.bookId]);
     } catch (err) {
       console.error(err);
     }
