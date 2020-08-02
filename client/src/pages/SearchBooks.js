b19cbd5d770d78e927a4a087cd764c593ee62608
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-08-02 11:46:44 -0700

    latest updates

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 import { SAVE_BOOK } from '../utils/mutations';
 import {useMutation} from '@apollo/react-hooks';
-//import { QUERY_BOOKS, GET_ME} from '../utils/queries';
+import { QUERY_BOOKS, GET_ME} from '../utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
